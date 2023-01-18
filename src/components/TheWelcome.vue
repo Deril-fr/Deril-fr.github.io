21b92a56f01038f22abdfcedfa88a34de299d641
garder500 <jeremy27.clara22@gmail.com>
2023-01-18 22:26:17 +0100

    Upload modifications

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,31 +12,25 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
+    <template #heading>Installation</template>
+      Vous aurez besoin d'installer l'extension NO-cors sur votre navigateur pour pouvoir utiliser l'application correctement. Voici les différent liens pour différent navigateurs :<br/>
+      <a href="https://chrome.google.com/webstore/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf" target="_blank" rel="noopener">Chrome</a><br/>
+      <a href="https://addons.mozilla.org/en-US/firefox/addon/access-control-allow-origin/" target="_blank" rel="noopener">Firefox</a><br/>
+      <a href="https://microsoftedge.microsoft.com/addons/detail/allow-cors-accesscontro/bhjepjpgngghppolkjdhckmnfphffdag" target="_blank" rel="noopener">Explorer</a><br/>
+      <a href="https://addons.opera.com/fr/extensions/details/cors-toggle/" target="_blank" rel="noopener">Opera</a>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>Tooling</template>
-
-    This project is served and bundled with
-    <a href="https://vitejs.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    recommended IDE setup is
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a> +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank" rel="noopener">Volar</a>. If
-    you need to test your components and web pages, check out
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">Cypress</a> and
-    <a href="https://on.cypress.io/component" target="_blank">Cypress Component Testing</a>.
-
-    <br />
-
-    More instructions are available in <code>README.md</code>.
+    <template #heading>Extensions optionnels</template>
+    Vous aurez peut être envie de télécharger vos épisodes sur votre ordinateur pour les regardez lors d'un voyage ?<br/>
+    Voici une extension qui vous permettra de télécharger vos épisodes directement sur votre ordinateur :<br/>
+    <a href="https://chrome.google.com/webstore/detail/live-stream-downloader/looepbdllpjgdmkpdcdffhdbmpbcfekj" target="_blank" rel="noopener">Live Stream Downloader(Chrome)</a><br/>
+    <a href="https://addons.mozilla.org/fr/firefox/addon/live-stream-downloader/" target="_blank" rel="noopener">Live Stream Downloader(Firefox)</a><br/>
+    <a href="https://operaextensions.com/extension/hls-downloader-extension-for-opera/" target="_blank" rel="noopener">Live Stream Downloader(Opera)</a><br/>
+    <a href="https://microsoftedge.microsoft.com/addons/detail/live-stream-downloader/ngjclnbcdbahekojpkhancmiobdahemb" target="_blank" rel="noopener">Live Stream Downloader(Explorer)</a><br/>
   </WelcomeItem>
 
   <WelcomeItem>
@@ -44,43 +38,22 @@ import SupportIcon from './icons/IconSupport.vue'
       <EcosystemIcon />
     </template>
     <template #heading>Ecosystem</template>
-
-    Get official tools and libraries for your project:
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
+      Ce site fonctionne essentiellement graçe à Neko-sama.fr sur les quelles sont présent l'intégralité des épisodes du site :<br/>
+      <a href="https://neko-sama.fr/" target="_blank" rel="noopener">Neko-sama.fr</a><br/>
+      <br/>
+      JapanWatch est un projet open-source, vous pouvez retrouver le code source sur github :<br/>
+      <a href="https://github.com/deril-fr" target="_blank" rel="noopener">Github</a><br/>
+      Une application mobile existe également (Playstore uniquement) :<br/>
+      <a href="https://play.google.com/store/apps/details?id=com.derilstream" target="_blank" rel="noopener">Playstore</a><br/>
   </WelcomeItem>
 
   <WelcomeItem>
     <template #icon>
       <CommunityIcon />
     </template>
-    <template #heading>Community</template>
-
-    Got stuck? Ask your question on
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>, our official
-    Discord server, or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >StackOverflow</a
-    >. You should also subscribe to
-    <a href="https://news.vuejs.org" target="_blank" rel="noopener">our mailing list</a> and follow
-    the official
-    <a href="https://twitter.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    twitter account for latest news in the Vue world.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>Support Vue</template>
-
-    As an independent project, Vue relies on community backing for its sustainability. You can help
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">becoming a sponsor</a>.
+    <template #heading>Communauté</template>
+      Des questions ? Des problèmes ?<br/>
+      Vous pouvez nous contacter sur notre serveur discord :<br/>
+      <a href="https://discord.gg/qHm7abf76p" target="_blank" rel="noopener">Discord</a><br/>
   </WelcomeItem>
 </template>
