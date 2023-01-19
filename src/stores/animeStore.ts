e76c398435237e12d225d4345a4667654129a679
garder500 <jeremy27.clara22@gmail.com>
2023-01-19 18:29:17 +0100

    Update

--- src/stores/animeStore.ts
+++
@@ -1,7 +0,0 @@
-import { atom, Atom } from "nanostores";
-import type {Anime} from "../types/Anime";
-
-export const animesVostFr = atom<Anime[]>([]);
-export const animeVostFr = atom<Anime[]>([]);
-
-export const ActualAnime = atom<Anime|null>(null);
