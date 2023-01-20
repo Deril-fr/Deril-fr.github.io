f527904a74668b5a8ec820d7d37d5b6a3c2f60bd
garder500 <jeremy27.clara22@gmail.com>
2023-01-20 01:53:54 +0100

    Updated website

--- src/utils/animehelper.ts
+++ src/utils/animehelper.ts
@@ -36,43 +36,6 @@ export default async function getM3U8(episodeUrl: string) {
     };
 }
 
-
-
-export async function getStreamtapeMP4(episodeUrl: string) {
-    const neko_data = await (await fetch(episodeUrl)).text();
-    console.log("Neko was fetched");
-    const streamtape_url = /(else {\n(.*)video\[0] = ')(.*)(';)/gm.exec(neko_data)?.[3] as string;
-
-    console.log("Streamtape url was fetched", streamtape_url);
-    const streamtape_data = await (await fetch(streamtape_url)).text();
-    const cloudflare_bypass = load(streamtape_data);
-    const form_url = cloudflare_bypass("#challenge-form").attr("action");
-    if (!form_url) return eval(/(document\.getElementById\(\'robotlink\'\)\.innerHTML = )(.*)(;)/gm.exec(streamtape_data)?.[2] as string);
-    const form_data = cloudflare_bypass("#challenge-form").serialize();
-    const streamtape_data2 = await (await fetch("https://streamtape.com/" + form_url, {
-        method: "POST",
-        headers: {
-            "UserAgent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.46",
-            "accept-encoding": " identity;q=1, *;q=0",
-            "content-type": "application/x-www-form-urlencoded",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-language": "en-US,en;q=0.9,fr;q=0.8",
-            "cache-control": "max-age=0",
-            "upgrade-insecure-requests": "1",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "sec-gpc": "1",
-            "te": "trailers"
-        },
-        body: form_data
-    })).text();
-
-    const cloudflare_bypass2 = eval(/(document\.getElementById\(\'robotlink\'\)\.innerHTML = )(.*)(;)/gm.exec(streamtape_data2)?.[2] as string);
-    return cloudflare_bypass2;
-}
-
 function convertHtmlToText(html: string) {
     const doc = new DOMParser().parseFromString(html, 'text/html');
     return doc.body.textContent || "";
