1d99ad7465fd09d231a1a056bcb3b1a8261eed3d
BuildTools <unconfigured@null.spigotmc.org>
2023-01-20 08:34:11 +0100

    fixed

--- src/views/EpisodeView.vue
+++ src/views/EpisodeView.vue
@@ -1,54 +1,57 @@
-<script setup lang="ts">
+<script lang="ts">
+import router from '@/router';
 import { animesStore } from '@/stores/animeStore';
 import { languageStore } from '@/stores/languageStore';
-import router from '@/router';
 import getM3U8, { getSynopsisAndEpisodes } from '@/utils/animehelper';
-import { ref, onUpdated, onBeforeMount, onMounted } from 'vue';
+import { ref, type Ref } from 'vue';
 import hls from 'hls.js';
 
-let currentEpisode = router.currentRoute.value.params.episode;
-let id = router.currentRoute.value.params.id;
-let player: HTMLVideoElement;
+export default {
+    data() {
+        return {
+            currentEpisode: router.currentRoute.value.params.episode,
+            id: router.currentRoute.value.params.id,
+            video: ref() as Ref<{
+                mp4: boolean;
+                uri: string;
+                available: boolean;
+                baseurl: string;
+            }>,
+        };
+    },
 
-const video = ref<{
-    mp4: boolean;
-    uri: string;
-    available: boolean;
-    baseurl: string;
-}>();
+    async mounted() {
+        let anime = animesStore[languageStore.language].find((anime) => anime.id.toString() == this.id);
+        if (!anime) return router.push('/');
 
-onMounted(async () => {
+        let data = await getSynopsisAndEpisodes(anime.url);
+        let episode = data.episodes.find((episode) => episode.episode.toString() == this.currentEpisode);
+        if (!episode) return router.push('/');
 
-    let anime = animesStore[languageStore.language].find((anime) => anime.id.toString() == id);
-    if (!anime) return router.push('/');
+        let m3u8 = await getM3U8('https://neko-sama.fr' + episode.url);
+        if (!m3u8) return router.push('/');
 
-    let data = await getSynopsisAndEpisodes(anime.url);
-    let episode = data.episodes.find((episode) => episode.episode.toString() == currentEpisode);
-    if (!episode) return router.push('/');
+        console.log(m3u8);
 
-    let m3u8 = await getM3U8('https://neko-sama.fr' + episode.url);
-    if (!m3u8) return router.push('/');
+        this.video = {
+            mp4: false,
+            uri: m3u8.uri,
+            available: true,
+            baseurl: m3u8.baseurl,
+        };
 
-    console.log(m3u8)
+        console.log(this.video);
 
-    video.value = {
-        mp4: false,
-        uri: m3u8.uri,
-        available: true,
-        baseurl: m3u8.baseurl,
-    };
+        if (this.video && this.video.available) {
+            let hlsPlayer = new hls();
 
-    console.log(video.value)
+            hlsPlayer.loadSource(this.video.uri);
+            hlsPlayer.attachMedia(this.$refs.player as HTMLMediaElement);
 
-    if (video.value && video.value.available) {
-        let hlsPlayer = new hls();
-
-        hlsPlayer.loadSource(video.value.uri);
-        hlsPlayer.attachMedia(player);
-
-        console.log(player)
-    }
-});
+            console.log(this.$refs.player as HTMLMediaElement);
+        }
+    },
+};
 </script>
 
 <template>
