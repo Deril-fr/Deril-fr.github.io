7ca4357ffe60267554fa57da5b82d265831fc14c
garder500 <jeremy27.clara22@gmail.com>
2023-02-01 02:08:52 +0100

    Updated website

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,5 +21,9 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const appCheck = initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider("6LdgX0EkAAAAAHQb4oD9X6IquPQcXWiFO_Q8-erj"),
+    isTokenAutoRefreshEnabled: true
+});
   
 export { auth, db, app };
