0165dba9e05eb20bda9d41d2259824f8cb6512ad
BuildTools <unconfigured@null.spigotmc.org>
2023-02-02 23:55:06 +0100

    Updated website

--- src/views/LastReleasedView.vue
+++ src/views/LastReleasedView.vue
@@ -1,26 +1,29 @@
 <script setup lang="ts">
 import LatestCardComponent from '@/components/LatestCardComponent.vue';
-import SearchBarComponent from '@/components/SearchBarComponent.vue';
 import { animesStore } from '@/stores/animeStore';
 import type LastEpisodes from '@/types/LastEpisodes';
 import type Anime from '@/types/Anime';
 import { getLastViewed } from '@/utils/animehelper';
 import { ref } from 'vue';
 
-let animeChunks = ref<{
-    animes: Anime;
-    lastEpisodes: LastEpisodes;
-}[]>();
+let animeChunks = ref<
+    {
+        animes: Anime;
+        lastEpisodes: LastEpisodes;
+    }[]
+>();
 getLastViewed().then((lastEpisodes) => {
     let animes = animesStore.all.filter((anime) => lastEpisodes.find((lastEpisode) => lastEpisode.anime_url === anime.url));
-    animeChunks.value = animes.map((anime) => {
-        return {
-            animes: anime,
-            lastEpisodes: lastEpisodes.find((lastEpisode) => lastEpisode.anime_url === anime.url) as LastEpisodes
-        };
-    }).sort((a, b) => {
-        return b.lastEpisodes.timestamp- a.lastEpisodes.timestamp;
-    });
+    animeChunks.value = animes
+        .map((anime) => {
+            return {
+                animes: anime,
+                lastEpisodes: lastEpisodes.find((lastEpisode) => lastEpisode.anime_url === anime.url) as LastEpisodes,
+            };
+        })
+        .sort((a, b) => {
+            return b.lastEpisodes.timestamp - a.lastEpisodes.timestamp;
+        });
 });
 </script>
 
@@ -36,7 +39,7 @@ getLastViewed().then((lastEpisodes) => {
         </div>
         <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-10">
             <div v-for="anime in animeChunks" :key="anime.animes.id + (anime.animes.lang as string)">
-                <LatestCardComponent :animeReal="anime.animes" :anime="anime.lastEpisodes"/>
+                <LatestCardComponent :animeReal="anime.animes" :anime="anime.lastEpisodes" />
             </div>
         </div>
     </section>
