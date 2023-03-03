27b20190c4a98d1f6892db63f55dda645c053a63
garder500 <jeremy27.clara22@gmail.com>
2023-03-03 11:40:44 +0100

    Updated website

--- src/utils/animehelper.ts
+++ src/utils/animehelper.ts
@@ -11,7 +11,6 @@ export default async function getM3U8(episodeUrl: string) {
     const pstream_url = /(\n(.*)video\[0] = ')(.*)(';)/gm.exec(neko_data)?.[3] as string;
     const pstream_data = await (await fetch(pstream_url)).text();
     // extract base url from pstream_url to extract online the origine of the url. It's the base url of the video 
-
     const baseurl = pstream_url.split("/").slice(0, 3).join("/");
     const loadedHTML = load(pstream_data);
     // get every script tag in the page
@@ -51,6 +50,22 @@ export default async function getM3U8(episodeUrl: string) {
                 // check if the script contains the subtitles
                 subtitlesvtt = pstream.subtitlesvtt;
                 break;
+            } else {
+                m3u8_url_B64 = /n=atob\("([^"]+)"/gm.exec(pstream_script)?.[1] as string;
+                if (m3u8_url_B64) {
+                    console.log(m3u8_url_B64);
+                    const b64 = JSON.parse(atob(m3u8_url_B64).replace(/\|\|\|/,"").slice(29));
+                    const pstream: PstreamData = b64;
+                    m3u8_url = Object.values(pstream).find((data: any) => {
+                        // check if data is a string
+                        if (typeof data === "string") {
+                            return data.startsWith("https://");
+                        }
+                    });
+                    // check if the script contains the subtitles
+                    subtitlesvtt = pstream.subtitlesvtt;
+                    break;
+                }
             }
         }
     }
@@ -76,7 +91,7 @@ export function convertEpisodeToNumber(episode: string) {
 }
 
 export async function getSynopsisAndEpisodes(url: string) {
-    const res = await fetch("https://neko-sama.fr" + url)
+    const res = await fetch("https://185.146.232.127" + url)
     const data = await res.text();
     const parsedData = /var episodes = (.+)\;/gm.exec(data);
     let episodes: EpisodeReal[] = [], synopsis: string = "", coverURL: string = "";
@@ -108,7 +123,7 @@ export async function getSynopsisAndEpisodes(url: string) {
 }
 
 export async function getLastViewed(): Promise<LastEpisodes[]> {
-    const res = await fetch("https://neko-sama.fr")
+    const res = await fetch("https://185.146.232.127")
     const data = await res.text();
     let lastEpisode: LastEpisodes[] = [];
     const parsedData = /var lastEpisodes = (.+)\;/gm.exec(data);
